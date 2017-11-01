bf5b90fd9bbded2eb472bcff3c1ae353e0463942
THUKey <304600203@qq.com>
2017-11-01 10:10:18 +0800

    add train.sh

---
+++ train.sh
@@ -0,0 +1,1 @@
+python cifar.py -a alexnet --epochs 164 --schedule 81 122 --gamma 0.1 --checkpoint checkpoints/cifar10/alexnet --data_root ~/Data/pytorch-data/cifar10-data/ --train-batch 4096 --gpu 0,1,2,3
